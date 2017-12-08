c5f0ed631ea2ebb3637f2770ec4ea6910a4d89c5
Andrew Branch <andrew.branch@jpl.nasa.gov>
2017-12-07 17:56:10 -0800

    Added rate timer to the UnderwaterVehicleSim node loop

--- underwater_vehicle_sim/src/underwater_vehicle_sim/UnderwaterVehicleSimNode.cpp
+++ underwater_vehicle_sim/src/underwater_vehicle_sim/UnderwaterVehicleSimNode.cpp
@@ -5,8 +5,11 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "underwater_vehicle_sim");
     ros::NodeHandle n;
 
+    ros::Rate r(100); //Hz at which to run the sim loop
+
     while(ros::ok())
     {
         ros::spinOnce();
+        r.sleep();
     }
 }
