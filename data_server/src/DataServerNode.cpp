65fbbe11f9729697f7de03e0c903ef93a763204d
Andrew Branch <andrew.branch@jpl.nasa.gov>
2018-01-17 15:22:57 -0800

    Added VehicleData message

--- data_server/src/DataServerNode.cpp
+++ data_server/src/DataServerNode.cpp
@@ -1,9 +1,14 @@
 #include "ros/ros.h"
 
 #include "data_server/DataServer.h"
-
+#include "underwater_vehicle_sim/VehicleData.h"
 
 DataServer server;
+
+bool recieveData(const underwater_vehicle_sim::VehicleData::ConstPtr& msg)
+{    
+
+}
 
 int main(int argc, char **argv)
 {
