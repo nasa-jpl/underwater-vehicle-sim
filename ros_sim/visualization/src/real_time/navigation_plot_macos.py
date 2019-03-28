b64fe0bbd9345a20d60417093aeb8ab59008fac8
Andrew Branch <andrew.branch@jpl.nasa.gov>
2019-03-27 18:37:36 -0700

    Added plotting of navigation filters
    Former-commit-id: 9e0406110d7e9e48f74af8f4a442aec086dbde95

---
+++ ros_sim/visualization/src/real_time/navigation_plot_macos.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+from std_msgs.msg import String
+from underwater_vehicle_msgs.msg import VehicleData
+from nav_msgs.msg import Odometry
+import math
+import numpy as np
+
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+import matplotlib.colors
+from matplotlib.animation import FuncAnimation
+
+
+
+xdata = []
+ydata = []
+scatters = []
+
+fig, ax = plt.subplots()
+ax.grid()
+
+def callback(data, args):
+    xdata[args[0]].append(data.pose.pose.position.x)
+    ydata[args[0]].append(data.pose.pose.position.y)
+
+def init():
+    ax.set_xlim(-50000,50000)
+    ax.set_ylim(-50000,50000)
+    return scatters
+
+def update(frame):
+    for x, y, s in zip(xdata, ydata, scatters):
+        if len(x) > 0:
+            s.set_offsets(zip(y, x))
+
+    return scatters
+
+def plot():
+    ani = FuncAnimation(fig, update, init_func=init, blit=True)
+    plt.show()
+    rospy.spin()
+
+def main():
+    rospy.init_node('real_time_map_view', anonymous=True)
+
+    for i, arg in enumerate(sys.argv[1:]):
+        rospy.Subscriber(arg, Odometry, callback, (i, ))
+        scatters.append(plt.scatter([], [], linewidth=0, animated=True))
+
+        xdata.append([])
+        ydata.append([])
+
+    plot()
+
+if __name__ == '__main__':
+    main()
