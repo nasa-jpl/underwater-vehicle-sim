2248d49c6ccf49d18ed0fb646eaa0692715d33b6
James Mason <james.mason@jpl.nasa.gov>
2019-12-30 16:39:45 -0800

    autorun trys to reap children with psutil
    Former-commit-id: 013c81b14ebdce93920c01885ecc55fc8b90a057

--- ros_sim/auto_run/src/auto_run.py
+++ ros_sim/auto_run/src/auto_run.py
@@ -12,9 +12,38 @@ import data_server.srv
 
 import argparse
 
+import psutil
+
 
 currentGoal = "running"
 dataFilePub = None
+
+def reap_children(timeout=3):
+    "Tries hard to terminate and ultimately kill all the children of this process."
+    def on_terminate(proc):
+        print("process {} terminated with exit code {}".format(proc, proc.returncode))
+
+    procs = psutil.Process().children()
+    # send SIGTERM
+    for p in procs:
+        try:
+            p.terminate()
+        except psutil.NoSuchProcess:
+            pass
+    gone, alive = psutil.wait_procs(procs, timeout=timeout, callback=on_terminate)
+    if alive:
+        # send SIGKILL
+        for p in alive:
+            print("process {} survived SIGTERM; trying SIGKILL".format(p))
+            try:
+                p.kill()
+            except psutil.NoSuchProcess:
+                pass
+        gone, alive = psutil.wait_procs(alive, timeout=timeout, callback=on_terminate)
+        if alive:
+            # give up
+            for p in alive:
+                print("process {} survived SIGKILL; giving up".format(p))
 
 def callback(data):
     global currentGoal
@@ -101,6 +130,9 @@ def runLaunchFile(uuid, filename, outputDirectory, inputDirectory):
     while not rospy.is_shutdown():
         print("Launch Nodes didn't shut down properly. Retrying")
         launch.shutdown()
+
+        print("Trying to reap children")
+        reap_children()
         time.sleep(2.5)
 
 
