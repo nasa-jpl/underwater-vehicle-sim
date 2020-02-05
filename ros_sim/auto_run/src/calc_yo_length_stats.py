fixed np median calculation
Former-commit-id: b83fb553d5962c1420b3bf242b5ad98c9686e04d

ros_sim/auto_run/src/calc_yo_length_stats.py
@@ -44,7 +44,7 @@ def main(bagName, ventX = 0, ventY = 0):
 	minYo = yoLengths.min()
 	maxYo = yoLengths.max()
 	stdYo = yoLengths.std()
-	medianYo = yoLengths.median()
+	medianYo = np.median(yoLengths)
 
 
 	print("Yo Length Statistics:")